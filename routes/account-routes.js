9c318ba0052df7c0b9b6b68598984aa7a811f8d9
ike barranco <barranco.ike@outlook.com>
2020-07-07 19:40:01 -0600

    moves account routes to different route page

--- routes/account-routes.js
+++ routes/account-routes.js
@@ -1,9 +1,36 @@
 const router = require("express").Router();
+const isAuthenticated = require("../config/middleware/isAuthenticated.js");
+const db = require("../models");
 const UserAccount = require("../models/userAccount.js");
+const passport = require("passport");
 
-router.get("/account/users", (req, res) => {
-    UserAccount.find({}).then(accounts => {
-        res.json(accounts);
+router.post("/account/signup", ({body}, res) => {
+    const newAccount = new UserAccount(body);
+    newAccount.beforeCreate();
+    newAccount.lastUpdatedDate();
+    console.log(newAccount);
+
+    db.UserAccount.create(newAccount)
+    .then(() => {
+        res.redirect(307, "/account/login");
+    })
+    .catch(err => {
+        res.status(401).json(err);
+    });
+});
+
+router.post("/account/login", passport.authenticate("local"), (req, res) => {
+    res.json(req.user);
+});
+
+router.get("/profile", isAuthenticated, (req, res) => {
+    db.UserAccount.findById({_id: req.user[0].id})
+    .then(async user => {
+        const profileInfo = await user.profileInfo()
+        await res.render("profile", {profile: profileInfo});
+    })
+    .catch(err => {
+        res.json(err);
     });
 });
 
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,4 @@
 const router = require("express").Router();
-const isAuthenticated = require("../config/middleware/isAuthenticated.js");
-const db = require("../models");
-const UserAccount = require("../models/userAccount.js");
-const passport = require("passport");
-
 
 router.get("/", (req, res) => {
     res.render("index");
@@ -15,36 +10,6 @@ router.get("/login", (req, res) => {
 
 router.get("/signup", (req, res) => {
     res.render("signup");
-});
-
-router.post("/account/signup", ({body}, res) => {
-    const newAccount = new UserAccount(body);
-    newAccount.beforeCreate();
-    newAccount.lastUpdatedDate();
-    console.log(newAccount);
-
-    db.UserAccount.create(newAccount)
-    .then(() => {
-        res.redirect(307, "/account/login");
-    })
-    .catch(err => {
-        res.status(401).json(err);
-    });
-});
-
-router.post("/account/login", passport.authenticate("local"), (req, res) => {
-    res.json(req.user);
-});
-
-router.get("/profile", isAuthenticated, (req, res) => {
-    db.UserAccount.findById({_id: req.user[0].id})
-    .then(async user => {
-        const profileInfo = await user.profileInfo()
-        await res.render("profile", {profile: profileInfo});
-    })
-    .catch(err => {
-        res.json(err);
-    });
 });
 
 module.exports = router;
