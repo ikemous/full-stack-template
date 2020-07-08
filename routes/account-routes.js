0abcff1bde3790d0e831208164bf86e36159e788
ike barranco <barranco.ike@outlook.com>
2020-07-07 20:04:02 -0600

    added comments to the routes script pages

--- routes/account-routes.js
+++ routes/account-routes.js
@@ -1,37 +1,62 @@
+//import dependencies
 const router = require("express").Router();
-const isAuthenticated = require("../config/middleware/isAuthenticated.js");
-const db = require("../models");
-const UserAccount = require("../models/userAccount.js");
 const passport = require("passport");
+//import authentication middleware
+const isAuthenticated = require("../config/middleware/isAuthenticated.js");
+//import databases
+const db = require("../models");
 
+
+/**
+ * /account/signup route
+ * Purpose: To sign up user and insert them into the database
+ */
 router.post("/account/signup", ({body}, res) => {
+    //create new user
     const newAccount = new UserAccount(body);
+    //hash password
     newAccount.beforeCreate();
+    //update updated date
     newAccount.lastUpdatedDate();
-    console.log(newAccount);
-
+    //Create user account
     db.UserAccount.create(newAccount)
     .then(() => {
+        //redirect to profile page
         res.redirect(307, "/account/login");
     })
     .catch(err => {
+        //change status and return json of err
         res.status(401).json(err);
     });
-});
+});//End /account/signup
 
+/**
+ * /account/login route
+ * Purpose: To login user to the website
+ */
 router.post("/account/login", passport.authenticate("local"), (req, res) => {
+    //return user information
     res.json(req.user);
-});
+});//End /account/login
 
+/**
+ * /profile
+ * Purpose: to render profile information
+ */
 router.get("/profile", isAuthenticated, (req, res) => {
+    //use user information to find database information
     db.UserAccount.findById({_id: req.user[0].id})
     .then(async user => {
-        const profileInfo = await user.profileInfo()
+        //grab user profile information
+        const profileInfo = await user.profileInfo();
+        //render profile page
         await res.render("profile", {profile: profileInfo});
     })
     .catch(err => {
+        //return json of err
         res.json(err);
     });
-});
+});//end /profile
 
+//export routes
 module.exports = router;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,15 +1,32 @@
+//import dependencies
 const router = require("express").Router();
 
+/**
+ * / route
+ * Purpose: To render Home Page
+ */
 router.get("/", (req, res) => {
+    //render index handlebars page
     res.render("index");
-});
+});// End /
 
+/**
+ * /login route
+ * Purpose: To render login page
+ */
 router.get("/login", (req, res) => {
+    //render login handlebars page
     res.render("login");
 });
 
+/**
+ * /signup route
+ * Purpose: Render signup page
+ */
 router.get("/signup", (req, res) => {
+    //render signup handlebars page
     res.render("signup");
-});
+});//End /signup
 
+//export routes
 module.exports = router;
